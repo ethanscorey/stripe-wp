1350e67954d7faed7ca4ad79d666864a1b483ce7
Ethan Corey <ethanscorey@gmail.com>
2023-10-20 16:39:22 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php // cash rules everything around me
---
+++ stripe-wp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * StripeWP
+ *
+ * @package   StripeWP
+ * @author    Ethan Corey
+ * @copyright 2023
+ * @license   MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name:       StripeWP
+ * Plugin URI:        https://github.com/ethanscorey/stripe-wp/
+ * Description:       A Stripe integration for WordPress.
+ * Version:           1.0.0
+ * Requires at least: 6.2
+ * Requires PHP:      7.2
+ * Author:            Ethan Corey
+ * Author URI:        https://github.com/ethanscorey/
+ * Text Domain:       stripe-wp
+ * License:           MIT
+ * License URI:       https://opensource.org/license/mit/
+ * Update URI:        https://github.com/ethanscorey/stripe-wp/releases
+ */
+namespace StripeWP;
