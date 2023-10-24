dd345f41657bc9636643b142dbe6f7a35aa5af04
Ethan Corey <ethanscorey@gmail.com>
2023-10-23 21:13:21 -0400

    Added hover colors in editor

--- blocks/src/price-option/edit.js
+++ blocks/src/price-option/edit.js
@@ -2,6 +2,7 @@ import { __ } from '@wordpress/i18n';
 import {
 	InspectorControls,
 	PanelColorSettings,
+	useBlockEditContext,
 	useBlockProps,
 } from '@wordpress/block-editor';
 import { TextControl } from '@wordpress/components';
@@ -19,41 +20,58 @@ import './editor.scss';
  * @return {Element} The price option component.
  */
 export default function Edit( { attributes, setAttributes } ) {
+	const { clientId } = useBlockEditContext();
 	return (
-		<div { ...useBlockProps() }>
-			<InspectorControls group="list">
-				<PanelColorSettings
-					title={ __( 'Hover Color', 'stripe-wp' ) }
-					initialOpen
-					colorSettings={ [
-						{
-							value: attributes.hoverTextColor,
-							onChange: ( value ) =>
-								setAttributes( { hoverTextColor: value } ),
-							label: __( 'Hover Text Color', 'stripe-wp' ),
-						},
-						{
-							value: attributes.hoverBackgroundColor,
-							onChange: ( value ) =>
-								setAttributes( {
-									hoverBackgroundColor: value,
-								} ),
-							label: __( 'Hover Background Color', 'stripe-wp' ),
-						},
-					] }
+		<>
+			<style>
+				#block-{ clientId }:hover, #block-{ clientId }:active, #block-
+				{ clientId }:focus { `{` }
+				background-color: { attributes.hoverBackgroundColor }{ ' ' }
+				!important; color: { attributes.hoverTextColor } !important;
+				{ `}` }
+			</style>
+			<div { ...useBlockProps() }>
+				<InspectorControls group="list">
+					<PanelColorSettings
+						title={ __( 'Hover Color', 'stripe-wp' ) }
+						initialOpen
+						colorSettings={ [
+							{
+								value: attributes.hoverTextColor,
+								onChange: ( value ) =>
+									setAttributes( { hoverTextColor: value } ),
+								label: __( 'Hover Text Color', 'stripe-wp' ),
+							},
+							{
+								value: attributes.hoverBackgroundColor,
+								onChange: ( value ) =>
+									setAttributes( {
+										hoverBackgroundColor: value,
+									} ),
+								label: __(
+									'Hover Background Color',
+									'stripe-wp'
+								),
+							},
+						] }
+					/>
+				</InspectorControls>
+				<span className="swp-d-none">{ __( 'Price option:' ) }</span>
+				<span className="wp-block-stripe-wp-price-option__currency">
+					$
+				</span>
+				<TextControl
+					type="number"
+					className="wp-block-stripe-wp-price-option__amount"
+					value={ attributes.unitAmount }
+					step="0.01"
+					min="0.01"
+					max="99_999.99"
+					onChange={ ( value ) =>
+						setAttributes( { unitAmount: value } )
+					}
 				/>
-			</InspectorControls>
-			<span className="swp-d-none">{ __( 'Price option:' ) }</span>
-			<span className="wp-block-stripe-wp-price-option__currency">$</span>
-			<TextControl
-				type="number"
-				className="wp-block-stripe-wp-price-option__amount"
-				value={ attributes.unitAmount }
-				step="0.01"
-				min="0.01"
-				max="99_999.99"
-				onChange={ ( value ) => setAttributes( { unitAmount: value } ) }
-			/>
-		</div>
+			</div>
+		</>
 	);
 }
